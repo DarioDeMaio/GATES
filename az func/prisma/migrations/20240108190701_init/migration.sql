5cc855bcbe9388acfefc0613e8ecb20929b86213
DarioDeMaio <demaio.dario95@gmail.com>
2024-01-10 23:43:40 +0100

    push device

--- az func/prisma/migrations/20240108190701_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Dispositivo" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "connectionString" TEXT NOT NULL,
-    "matricola" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Dispositivo_matricola_key" ON "Dispositivo"("matricola");
