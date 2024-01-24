882d95ae5b53eb0491a17b44b8d1c944c5640d9e
DarioDeMaio <demaio.dario95@gmail.com>
2024-01-24 19:55:16 +0100

    push test

--- az func/script.ts
+++ az func/script.ts
@@ -4,7 +4,8 @@ const prisma = new PrismaClient()
 
 async function main() {
     //prisma.dispositivo.findMany();
-    for(let i = 0; i < 5; i++){
+    let i = 0;
+    for(; i < 1; i++){
         const misurazione = await prisma.misurazioniAcqua.create({
             data:{
                 pH:22,
@@ -12,9 +13,25 @@ async function main() {
                 dispId:13
             }
         });
-    
-  console.log(misurazione);
+        console.log(misurazione);
+        //let str:String = misurazione.data.toLocaleString('it-IT', { timeZone: 'UTC'});
+      
     }
+    const getD = await prisma.misurazioniAcqua.findMany();
+    for(let i = 0; i < getD.length; i++){
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var d = new Date(getD[i].data.getDate());
+      var dayName = days[d.getDay()];
+      let str:string = getD[i].data.getFullYear()+"-"+(getD[i].data.getMonth()+1).toString()+"-"+getD[i].data.getDate().toString();
+      console.log(str,"\n");
+      var d = new Date(str);
+      console.log(getD[i].data.getDate());
+      // console.log(getD[i],"\n\n");
+      // console.log(d.getDay());
+    }
+    // let str:String = getD[i].data.toLocaleString('it-IT', { timeZone: 'UTC'});
+    // var d = new Date(getD[i].data.getDate());
+    // console.log(d.getDay());
 }
 
 main()
