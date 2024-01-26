191b29bb98f5155eae0d11b7d94fd94326f43099
DarioDeMaio <demaio.dario95@gmail.com>
2024-01-26 10:37:13 +0100

    fix

--- az func/script.ts
+++ az func/script.ts
@@ -17,10 +17,9 @@ async function main() {
     },
   });
 
-  // Genera almeno 10 misurazioni per ogni mese
   for (let month = 1; month <= 12; month++) {
     for (let j = 0; j < 10; j++) {
-      // Imposta la data correttamente
+      
       const data = new Date(`2024-${month.toString().padStart(2, '0')}-${(j + 1).toString().padStart(2, '0')}T18:25:26.216Z`);
 
       const misurazione = await prisma.misurazioniAria.create({
