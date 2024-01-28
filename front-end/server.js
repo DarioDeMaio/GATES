d3bfd282319257604d59f2b9c9398b427dbfe5fb
DarioDeMaio <demaio.dario95@gmail.com>
2024-01-28 18:27:06 +0100

    test

---
+++ front-end/server.js
@@ -0,0 +1,24 @@
+// const express = require('express')
+// const app = express()
+// const path = require('path');
+
+// app.get('/', async (req, res) => {
+//     res.sendFile(path.join(__dirname, 'html/index.html'));
+// });
+
+// app.get('/logo', async (req, res) => {
+//     res.sendFile(path.join(__dirname, 'images/GATES.png'));
+// });
+
+// app.get('/add_device', async (req, res) => {
+//     res.sendFile(path.join(__dirname, 'html/add_device.html'));
+// });
+
+// app.get('/navbar', async (req, res) => {
+//     res.sendFile(path.join(__dirname, 'html/navbar.html'));
+// });
+
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
