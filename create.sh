cf6f2aaa902586723c99bbb7ed00823eb0faa872
DarioDeMaio <115263219+DarioDeMaio@users.noreply.github.com>
2024-01-31 23:24:48 +0100

    push

--- create.sh
+++ create.sh
@@ -4,15 +4,17 @@ server="gates"
 shadowDB="gatesShadow"
 database="gates"
 login="gates"
-password="#"
-iot_hub="gates-iot-hub"
+password="Giulia-dario1"
+iot_hub="gatess"
 function_app="gatess"
 storage_account="gaates"
 stream_job="gates_job"
 startIp=0.0.0.0
 endIp=255.255.255.255
 iot_hub_policy_name="iothubowner"
-iot_hub_policy_key=$(az iot hub policy show --hub-name $IOT_HUB -n $iot_hub_policy_name | jq -r '.primaryKey')
+
+echo "Login"
+az login
 
 echo "Creating resource group: $resourceGroup...."
 az group create --name $resourceGroup --location $location
@@ -32,10 +34,14 @@ az sql db create --resource-group $resourceGroup --server $server --name $shadow
 echo "Creating the IoT Hub $iot_hub"
 az iot hub create -n $iot_hub --resource-group $resourceGroup --sku F1 --partition-count 2 --location $location
 
-echo IOT_HUB_CONNECTION_STRING="$( az iot hub connection-string show --hub-name $iot_hub --resource-group $resourceGroup | jq '.connectionString' )" >> az_func/.env
 
-echo "DATABASE_URL=\"sqlserver://$server.database.windows.net:1433;database=$database;user=$login@$login;password=$password\"" >> az_func/.env
-echo "SHADOW_DATABASE_URL=\"sqlserver://$server.database.windows.net:1433;database=$shadowDB;user=$login@$login;password=$password\"" >> az_func/.env
+iot_hub_policy_key=$(az iot hub policy show --hub-name $iot_hub -n $iot_hub_policy_name | jq -r '.primaryKey')
+
+echo "" >> "az func"/.env
+# echo IOT_HUB_CONNECTION_STRING="$( az iot hub connection-string show --hub-name $iot_hub --resource-group $resourceGroup | jq '.connectionString' )" >> "az func"/.env
+
+echo "DATABASE_URL=\"sqlserver://$server.database.windows.net:1433;database=$database;user=$login@$login;password=$password\"" >> "az func"/.env
+echo "SHADOW_DATABASE_URL=\"sqlserver://$server.database.windows.net:1433;database=$shadowDB;user=$login@$login;password=$password\"" >> "az func"/.env
 
 echo "Creating the storage account $storage_account"
 az storage account create --name $storage_account --resource-group $resourceGroup --location $location --sku Standard_LRS --kind StorageV2
@@ -43,47 +49,47 @@ az storage account create --name $storage_account --resource-group $resourceGrou
 echo "Creating the function app $function_app"
 az functionapp create --name $function_app --storage-account $storage_account --resource-group $resourceGroup --consumption-plan-location $location --runtime node --runtime-version 18 --functions-version 4 --os-type Linux
 
-# echo "Creating stream analytics job $stream_job"
-# az stream-analytics job create --name $stream_job --resource-group $resourceGroup --location $location --transformation name="transformationSAJ" streaming-units=1
 
 
-cd az_func
+cd "az func"
 npx prisma generate
 npx prisma db push
 func azure functionapp publish $function_app
 
+# echo "Creating stream analytics job $stream_job"
+# az stream-analytics job create --name $stream_job --resource-group $resourceGroup --location $location --transformation name="transformationSAJ" streaming-units=1
 
-# Configura l'input per lo Stream Analytics Job
-input_properties=$(cat <<EOF
-{
-  "type": "Stream",
-  "datasource": {
-    "type": "Microsoft.Devices/IotHubs",
-    "properties": {
-      "consumerGroupName": "\$Default",
-      "endpoint": "messages/events",
-      "iotHubNamespace": "$iot_hub",
-      "sharedAccessPolicyKey": "$iot_hub_policy_key",
-      "sharedAccessPolicyName": "$iot_hub_policy_name"
-    }
-  },
-  "serialization": {
-    "type": "Json",
-    "properties": {
-      "encoding": "UTF8"
-    }
-  }
-}
-EOF
-)
+# # Configura l'input per lo Stream Analytics Job
+# input_properties=$(cat <<EOF
+# {
+#   "type": "Stream",
+#   "datasource": {
+#     "type": "Microsoft.Devices/IotHubs",
+#     "properties": {
+#       "consumerGroupName": "\$Default",
+#       "endpoint": "messages/events",
+#       "iotHubNamespace": "$iot_hub",
+#       "sharedAccessPolicyKey": "$iot_hub_policy_key",
+#       "sharedAccessPolicyName": "$iot_hub_policy_name"
+#     }
+#   },
+#   "serialization": {
+#     "type": "Json",
+#     "properties": {
+#       "encoding": "UTF8"
+#     }
+#   }
+# }
+# EOF
+# )
 
-echo "Creating input for Stream Analytics Job $stream_job"
-az stream-analytics input create --name $IOT_HUB --job-name $stream_job --resource-group $RESOURCE_GROUP --properties "$input_properties"
+# echo "Creating input for Stream Analytics Job $stream_job"
+# az stream-analytics input create --name $IOT_HUB --job-name $stream_job --resource-group $RESOURCE_GROUP --properties "$input_properties"
 
-#Configura l'output per lo Stream Analytics Job
+# #Configura l'output per lo Stream Analytics Job
 
-echo "Creating output for Stream Analytics Job $stream_job: $database"
-az stream-analytics output create --job-name $stream_job --datasource "{\"type\":\"Microsoft.Sql/Server/Database\",\"properties\":{\"server\":\"$server\",\"database\":\"$database\",\"user\":\"$login\",\"password\":\"$password\",\"table\":\"CollectedData\"}}" --serialization "{\"type\":\"Json\",\"properties\":{\"format\":\"Array\",\"encoding\":\"UTF8\"}}" --output-name gates --resource-group $resourceGroup
+# echo "Creating output for Stream Analytics Job $stream_job: $database"
+# az stream-analytics output create --job-name $stream_job --datasource "{\"type\":\"Microsoft.Sql/Server/Database\",\"properties\":{\"server\":\"$server\",\"database\":\"$database\",\"user\":\"$login\",\"password\":\"$password\",\"table\":\"CollectedData\"}}" --serialization "{\"type\":\"Json\",\"properties\":{\"format\":\"Array\",\"encoding\":\"UTF8\"}}" --output-name gates --resource-group $resourceGroup
 
 
 
